975cec84f2827d8a5ac56885c51622f136f6e5e4
伊藤 夏輝(ＣＴ５) <nat-itoh@cec-ltd.co.jp>
2024-10-25 19:36:33 +0900

    Unityプロジェクトを追加．

---
+++ Assets/com.nitou.BlockPG/Scripts/DragDrop/I_BPG_Spot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace nitou.BlockPG{
+
+    public interface I_BPG_Spot {
+
+    }
+
+    /// <summary>
+    /// <see cref="I_BPG_Spot"/>型の基本的な拡張メソッド集．
+    /// </summary>
+    public static class BPG_Spot_Extensions {
+
+    }
+}
